3ca209e70878146ff512c04515f66ab43b64a27c
wilfft <68745408+wilfft@users.noreply.github.com>
2021-01-17 11:20:04 -0300

    installing browser router

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import "./App.css";
 import UsersList from "./Containers/UsersList/UsersList";
 
 axios.defaults.baseURL = "https://shawn-back.herokuapp.com";
+//axios.defaults.baseURL = "http://localhost:2000";
 
 class App extends React.Component {
   render() {
